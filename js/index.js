8d4bc037e73c73dc489336e16c3659fc47cbfd09
alexandru_marincas <alexandru.marincas@yahoo.ro>
2021-12-24 09:19:58 +0200

    handled special border case for the roof

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const elements = document.querySelectorAll('.el');
 const toggleBorders = document.querySelector('#toggleBorders');
+const roof = document.querySelector('.roof');
 
 toggleBorders.addEventListener('change', function(){
     if(this.checked){
         elements.forEach(el => el.classList.add('border'));
+        roof.classList.add('el-special');
     }else{
         elements.forEach(el => el.classList.remove('border'));
+        roof.classList.remove('el-special');
     }
 });
