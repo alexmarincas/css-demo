a6b048973dbbd515df5882399b8fd4bc1e83adc0
alexandru_marincas <alexandru.marincas@yahoo.ro>
2021-12-24 09:29:48 +0200

    added dynamic title to the checkbox after each state change

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ const elements = document.querySelectorAll('.el');
 const toggleBorders = document.querySelector('#toggleBorders');
 const roof = document.querySelector('.roof');
 
-toggleBorders.addEventListener('change', function(){
-    if(this.checked){
+toggleBorders.addEventListener('change', (e)=>{
+    const el = e.target;
+    if(el.checked){
         elements.forEach(el => el.classList.add('border'));
         roof.classList.add('el-special');
+        e.path[1].setAttribute('data-title', 'Hide borders');
     }else{
         elements.forEach(el => el.classList.remove('border'));
         roof.classList.remove('el-special');
+        e.path[1].setAttribute('data-title', 'Show borders');
     }
 });
